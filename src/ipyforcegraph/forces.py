1e38ce7cac36259f35aeea178a15ed601ff3512e
Dane Freeman <dane.freeman@gtri.gatech.edu>
2023-02-06 19:29:48 -0500

    gh-28 more python docstrings for forces.

--- src/ipyforcegraph/forces.py
+++ src/ipyforcegraph/forces.py
@@ -69,19 +69,25 @@ class LinkForce(BaseD3Force):
 
 @W.register
 class CenterForce(BaseD3Force):
+    """The centering force translates nodes uniformly so that the mean position
+    of all nodes (center of mass if all nodes have equal weight) is at the given
+    position (x, y, z).
+
+    https://github.com/d3/d3-force#centering
+    """
     _model_name: str = T.Unicode("CenterForceModel").tag(sync=True)
     key: str = T.Unicode("center").tag(sync=True)
-    x: Optional[int] = T.Float(
+    x: Optional[float] = T.Float(
         None,
         allow_none=True,
         help="sets the x-coordinate of the centering position to the specified number and returns this force.",
     ).tag(sync=True)
-    y: Optional[int] = T.Float(
+    y: Optional[float] = T.Float(
         None,
         allow_none=True,
         help="sets the y-coordinate of the centering position to the specified number and returns this force.",
     ).tag(sync=True)
-    z: Optional[int] = T.Float(
+    z: Optional[float] = T.Float(
         None,
         allow_none=True,
         help="sets the z-coordinate of the centering position to the specified number and returns this force.",
@@ -90,28 +96,129 @@ class CenterForce(BaseD3Force):
 
 @W.register
 class XForce(BaseD3Force):
+    """The X position force push nodes towards a desired position along the
+    given dimension with a configurable strength.
+
+    https://github.com/d3/d3-force#positioning
+    """
     _model_name: str = T.Unicode("XForceModel").tag(sync=True)
     x: Optional[int] = T.Float(
         None,
         allow_none=True,
         help="sets the x-coordinate of the centering position to the specified number and returns this force.",
     ).tag(sync=True)
+    strength: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate strength. Context takes `node`",
+    ).tag(sync=True)
+
 
 
 @W.register
 class YForce(BaseD3Force):
+    """The Y position force push nodes towards a desired position along the
+    given dimension with a configurable strength.
+
+    https://github.com/d3/d3-force#positioning
+    """
     _model_name: str = T.Unicode("YForceModel").tag(sync=True)
     y: Optional[int] = T.Float(
         None,
         allow_none=True,
         help="sets the y-coordinate of the centering position to the specified number and returns this force.",
     ).tag(sync=True)
+    strength: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate strength. Context takes `node`",
+    ).tag(sync=True)
 
 
 @W.register
 class ZForce(BaseD3Force):
+    """The Z position force push nodes towards a desired position along the
+    given dimension with a configurable strength.
+
+    https://github.com/d3/d3-force#positioning
+    """
     _model_name: str = T.Unicode("ZForceModel").tag(sync=True)
     z: Optional[int] = T.Float(
+        None,
+        allow_none=True,
+        help="sets the z-coordinate of the centering position to the specified number and returns this force.",
+    ).tag(sync=True)
+    strength: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate strength. Context takes `node`",
+    ).tag(sync=True)
+
+
+@W.register
+class ManyBodyForce(BaseD3Force):
+    """The many-body (or n-body) force applies mutually amongst all nodes. It
+    can be used to simulate gravity (attraction) if the strength is positive, or
+    electrostatic charge (repulsion) if the strength is negative. This
+    implementation uses quadtrees and the Barnes–Hut approximation to greatly
+    improve performance; the accuracy can be customized using the theta
+    parameter.
+
+    https://github.com/d3/d3-force#many-body
+    """
+    _model_name: str = T.Unicode("ManyBodyForceModel").tag(sync=True)
+    strength: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate strength. Context takes `node`",
+    ).tag(sync=True)
+    theta: Optional[float] = T.Unicode(
+        Node,
+        allow_node=True,
+        help="sets the Barnes–Hut approximation criterion to the specified number and returns this force."
+    )
+    distance_min: Optional[float] = T.Unicode(
+        Node,
+        allow_node=True,
+        help="sets the minimum distance between nodes over which this force is considered."
+    )
+    distance_man: Optional[float] = T.Unicode(
+        Node,
+        allow_node=True,
+        help="sets the maximum distance between nodes over which this force is considered."
+    )
+
+
+@W.register
+class RadialForce(BaseD3Force):
+    """The radial positioning force create a force towards a circle of the
+    specified radius centered at (x, y).
+    https://github.com/d3/d3-force#forceRadial
+    """
+
+    _model_name: str = T.Unicode("RadialForceModel").tag(sync=True)
+    radius: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate radius. Context takes `node`",
+    ).tag(sync=True)
+
+    strength: Optional[str] = T.Unicode(
+        None,
+        allow_none=True,
+        help="a nunjucks template to use to calculate strength. Context takes `node`",
+    ).tag(sync=True)
+    x: Optional[float] = T.Float(
+        None,
+        allow_none=True,
+        help="sets the x-coordinate of the centering position to the specified number and returns this force.",
+    ).tag(sync=True)
+    y: Optional[float] = T.Float(
+        None,
+        allow_none=True,
+        help="sets the y-coordinate of the centering position to the specified number and returns this force.",
+    ).tag(sync=True)
+    z: Optional[float] = T.Float(
         None,
         allow_none=True,
         help="sets the z-coordinate of the centering position to the specified number and returns this force.",
@@ -119,20 +226,11 @@ class ZForce(BaseD3Force):
 
 
 @W.register
-class ManyBodyForce(BaseD3Force):
-    _model_name: str = T.Unicode("ManyBodyForceModel").tag(sync=True)
-
-
-@W.register
-class RadialForce(BaseD3Force):
-    """[](https://github.com/d3/d3-force#collision)"""
-
-    _model_name: str = T.Unicode("RadialForceModel").tag(sync=True)
-
-
-@W.register
 class CollisionForce(BaseD3Force):
-    """[](https://github.com/d3/d3-force#collision)"""
+    """The collision force treats nodes as circles with a given `radius`, rather
+    than points and prevents nodes from overlapping.
+    https://github.com/d3/d3-force#collision
+    """
 
     _model_name: str = T.Unicode("CollisionForceModel").tag(sync=True)
     radius: Optional[str] = T.Unicode(
