6c959aadea37bd5223a77b47ed7f9a352eb6a82c
Dane Freeman <dane.freeman@gtri.gatech.edu>
2023-02-07 10:56:24 -0500

    gh-28 connecting radial force

--- js/widgets/behaviors/forces/force-radial.ts
+++ js/widgets/behaviors/forces/force-radial.ts
@@ -22,4 +22,40 @@ export class RadialForceModel extends ForceBehaviorModel implements IBehave, IFo
   forceFactory(): d3ForceRadial {
     return d3ForceRadial();
   }
+
+  get force(): d3ForceRadial {
+    const { strength, radius, x, y, z } = this;
+
+    let force =this._force;
+    force = strength == null ? force : force.strength(strength);
+    force = radius == null ? force : force.radius(this.radius);
+    force = x == null ? force : force.x(x);
+    force = y == null ? force : force.y(y);
+    force = z == null ? force : force.z(z);
+    return force;
+  }
+
+  get triggerChanges(): string {
+    return "change:strength change:radius change:x change:y change:z";
+  }
+
+  get strength() {
+    return this.get('strength');
+  }
+
+  get radius() {
+    return this.get('radius');
+  }
+
+  get x() {
+    return this.get('x');
+  }
+
+  get y() {
+    return this.get('y');
+  }
+
+  get z() {
+    return this.get('z');
+  }
 }
