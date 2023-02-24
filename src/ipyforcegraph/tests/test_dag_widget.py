618f39d285a555010af162c65169b3c92ade1f23
Dane Freeman <dane.freeman@gtri.gatech.edu>
2023-02-24 10:56:24 -0500

    gh-33 fix dag api check test

--- src/ipyforcegraph/tests/test_dag_widget.py
+++ src/ipyforcegraph/tests/test_dag_widget.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2023 ipyforcegraph contributors.
 # Distributed under the terms of the Modified BSD License.
 
-from ..behaviors import DAGBehavior
+from ..behaviors.forces import DAGForce
 
 
 def test_widget_source() -> None:
-    behavior = DAGBehavior(active=False)
+    behavior = DAGForce(active=False)
 
     assert not behavior.active
     assert behavior.node_filter == ""
