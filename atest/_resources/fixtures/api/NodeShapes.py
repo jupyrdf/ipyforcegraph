cad04a82396ad971941fb244641e922d2bfd9af1
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2023-03-06 10:48:17 -0600

    try more tricks for rectangle tests

--- atest/_resources/fixtures/api/NodeShapes.py
+++ atest/_resources/fixtures/api/NodeShapes.py
@@ -2,11 +2,13 @@ from ipyforcegraph.graphs import WIDGET_CLASS
 from ipyforcegraph import behaviors as B
 from ipyforcegraph.behaviors import shapes as S
 
-transparent = """rgba(0,0,0,0.0)"""
+transparent = """rgba(255,255,255,0.0)"""
 shape = S.SHAPE_CLASS(fill=transparent)
 
 if isinstance(shape, S.Text):
     shape.text = B.Column("id")
+elif isinstance(shape, S.Rectangle):
+    shape.opacity = 0
 
 shape.FEATURE = B.Nunjucks(transparent)
 
