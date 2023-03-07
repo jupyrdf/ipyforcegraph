b1b9352778113f5e50c99dcb46932d11ed18db60
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2023-03-07 10:15:49 -0500

    started the ColumnOrTemplate-ectomy, everything is broken!

--- js/widgets/behaviors/base.ts
+++ js/widgets/behaviors/base.ts
@@ -2,23 +2,12 @@
  * Copyright (c) 2023 ipyforcegraph contributors.
  * Distributed under the terms of the Modified BSD License.
  */
-import type { Template } from 'nunjucks';
-
 import { ISignal, Signal } from '@lumino/signaling';
 
 import { IBackboneModelOptions, WidgetModel } from '@jupyter-widgets/base';
 
 import { newTemplate } from '../../template-utils';
-import {
-  ECoerce,
-  EMOJI,
-  IBehave,
-  IHasGraph,
-  ILinkBehaveOptions,
-  INodeBehaveOptions,
-  TUpdateKind,
-  WIDGET_DEFAULTS,
-} from '../../tokens';
+import { ECoerce, IBehave, TUpdateKind, WIDGET_DEFAULTS } from '../../tokens';
 import { getCoercer, noop } from '../../utils';
 
 export class BehaviorModel extends WidgetModel implements IBehave {
@@ -103,111 +92,5 @@ export class ColumnModel extends DynamicModel {
     const coercer = getCoercer(this.coerce);
     this._nodeHandler = (options: any) => coercer(options.node[value]);
     this._linkHandler = (options: any) => coercer(options.link[value]);
-  }
-}
-
-export class ColumnOrTemplateModel extends BehaviorModel implements IBehave {
-  protected _nunjucksTemplate: Template | null;
-
-  defaults() {
-    return {
-      ...super.defaults(),
-      ...WIDGET_DEFAULTS,
-      column_name: null,
-      template: null,
-    };
-  }
-
-  getColumnName(hasGraph: IHasGraph): string | null {
-    return this.get('column_name') || null;
-  }
-
-  get template(): string | null {
-    return this.get('template') || null;
-  }
-
-  initialize(attributes: Backbone.ObjectHash, options: IBackboneModelOptions) {
-    super.initialize(attributes, options);
-    this.on('change:column_name', this.onColumnNameChange, this);
-    this.on('change:template', this.onTemplateChange, this);
-    void this.onColumnNameChange();
-    void this.onTemplateChange();
-  }
-
-  protected async onTemplateChange(): Promise<void> {
-    const { template } = this;
-    if (template == null) {
-      this._nunjucksTemplate = null;
-    } else {
-      try {
-        this._nunjucksTemplate = await newTemplate(template);
-      } catch (err) {
-        console.warn(EMOJI, err['message']);
-        this._nunjucksTemplate = null;
-      }
-    }
-    this._updateRequested.emit(void 0);
-  }
-
-  protected onColumnNameChange(change?: any) {
-    this._updateRequested.emit(void 0);
-  }
-
-  renderTemplate(options: any): string | null {
-    const { _nunjucksTemplate } = this;
-
-    if (_nunjucksTemplate != null) {
-      try {
-        return _nunjucksTemplate.render(options);
-      } catch (err) {
-        console.warn(EMOJI, err);
-      }
-    }
-  }
-}
-
-export class NodeColumnOrTemplateModel
-  extends ColumnOrTemplateModel
-  implements IBehave
-{
-  protected _nunjucksTemplate: Template | null;
-
-  protected getNodeAttr(options: INodeBehaveOptions): string | null {
-    let value = this.renderTemplate(options);
-
-    if (value != null) {
-      return value;
-    }
-
-    const columnName = this.getColumnName(options.view);
-
-    if (columnName != null) {
-      value = options.node[columnName];
-    }
-
-    return value || null;
-  }
-}
-
-export class LinkColumnOrTemplateModel
-  extends ColumnOrTemplateModel
-  implements IBehave
-{
-  protected _nunjucksTemplate: Template | null;
-
-  protected getLinkAttr(options: ILinkBehaveOptions): string | null {
-    let value = this.renderTemplate(options);
-
-    if (value != null) {
-      return value;
-    }
-
-    const columnName = this.getColumnName(options.view);
-
-    if (columnName != null) {
-      value = options.link[columnName];
-    }
-
-    return value || null;
   }
 }
--- js/widgets/behaviors/node-tooltip.ts
+++ js/widgets/behaviors/node-tooltip.ts
@@ -2,30 +2,61 @@
  * Copyright (c) 2023 ipyforcegraph contributors.
  * Distributed under the terms of the Modified BSD License.
  */
-import { IBehave, IHasGraph, INodeBehaveOptions } from '../../tokens';
+import {
+  IBackboneModelOptions,
+  unpack_models as deserialize,
+} from '@jupyter-widgets/base';
 
-import { NodeColumnOrTemplateModel } from './base';
+import { IBehave } from '../../tokens';
 
-export class NodeLabelModel extends NodeColumnOrTemplateModel implements IBehave {
-  static model_name = 'NodeLabelModel';
+import { BehaviorModel } from './base';
+
+export class NodeTooltipModel extends BehaviorModel implements IBehave {
+  static model_name = 'NodeTooltipModel';
+  static serializers = {
+    ...BehaviorModel.serializers,
+    label: { deserialize },
+  };
 
   defaults() {
-    return { ...super.defaults(), _model_name: NodeLabelModel.model_name };
+    return {
+      ...super.defaults(),
+      _model_name: NodeTooltipModel.model_name,
+      label: 'id',
+    };
   }
 
-  columnName(hasGraph: IHasGraph): string | null {
-    let columnName = super.getColumnName(hasGraph);
-    if (columnName == null) {
-      try {
-        columnName = (hasGraph.source as any).get('node_id_column');
-      } catch (err) {
-        console.error('failed to fetch id column', err);
-      }
+  initialize(attributes: Backbone.ObjectHash, options: IBackboneModelOptions) {
+    super.initialize(attributes, options);
+    this.on('change:label', this.onLabelChanged, this);
+    void this.onLabelChanged.call(this);
+  }
+
+  async onLabelChanged() {
+    for (const label of this.label) {
+      await label.ensureFacets();
+      label.updateRequested.connect(this.onLabelChanged, this);
     }
-    return columnName || null;
+    this._updateRequested.emit(void 0);
   }
 
-  getNodeLabel(options: INodeBehaveOptions): string | null {
-    return super.getNodeAttr(options);
+  get label(): String {
+    return this.get('label') || 'id';
   }
+
+  // columnName(hasGraph: IHasGraph): string | null {
+  //   let columnName = super.getColumnName(hasGraph);
+  //   if (columnName == null) {
+  //     try {
+  //       columnName = (hasGraph.source as any).get('node_id_column');
+  //     } catch (err) {
+  //       console.error('failed to fetch id column', err);
+  //     }
+  //   }
+  //   return columnName || null;
+  // }
+
+  // getNodeLabel(options: INodeBehaveOptions): string | null {
+  //   return super.getNodeAttr(options);
+  // }
 }
