ddf9829780ea4d9afe37484d85aa05b6f5eb1701
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2023-04-01 16:30:26 -0400

    adding default values to particles behavior, and making the emit method work using Nunjucks templating

--- src/ipyforcegraph/behaviors/particles.py
+++ src/ipyforcegraph/behaviors/particles.py
@@ -3,13 +3,14 @@
 # Copyright (c) 2023 ipyforcegraph contributors.
 # Distributed under the terms of the Modified BSD License.
 
+from time import sleep
 from typing import Any, Optional
 
 import ipywidgets as W
 import traitlets as T
 
 from ..trait_utils import JSON_TYPES, coerce
-from ._base import Behavior, TFeature, TNumFeature, _make_trait
+from ._base import Behavior, Nunjucks, TFeature, TNumFeature, _make_trait
 
 
 @W.register
@@ -21,13 +22,15 @@ class LinkParticles(Behavior):
        or they will exceed the frame rate of the animation.
     """
 
-    DEFAULT_SPEED = 0.1
+    DEFAULT_SPEED = 0.02
 
     _model_name: str = T.Unicode("LinkParticleModel").tag(sync=True)
 
-    color: TFeature = _make_trait("the color of the particles")
+    color: TFeature = _make_trait("the color of the particles", default_value="black")
     density: TNumFeature = _make_trait(
-        "the number of particles, ideally 0.0 < ``value``", numeric=True
+        "the number of particles, ideally 0.0 < ``value``",
+        numeric=True,
+        default_value=1,
     )
     speed: Optional[TNumFeature] = _make_trait(
         "the speed of the particles, ideally 0.0 < ``value`` < ~0.1",
@@ -35,17 +38,43 @@ class LinkParticles(Behavior):
         default_value=DEFAULT_SPEED,
     )
     width: TNumFeature = _make_trait(
-        "the size of the particles, ideally 0.0 < ``value`` < ~5", numeric=True
+        "the size of the particles, ideally 0.0 < ``value`` < ~5",
+        numeric=True,
+        default_value=3,
     )
 
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self._original_speed: TNumFeature = None
 
-    def emit(self, *links: int) -> None:
+    def emit(
+        self, *link_indeces: int, speed: Optional[float] = None, duration: float = 1
+    ) -> None:
         """Emit particles over a series of links"""
-        for link in links:
-            self.send({"action": "emitParticles", "link": link})
+        stopped = False
+
+        if speed is None:
+            if isinstance(self.speed, (float, int)):
+                speed = self.speed
+            else:
+                speed = self.DEFAULT_SPEED
+
+        if self.speed:
+            stopped = True
+            self.stop()
+
+        self.speed = Nunjucks(
+            "{% if link.index in "
+            + str([int(idx) for idx in link_indeces])
+            + "%}"
+            + str(speed)
+            + "{% endif %}"
+        )
+
+        if stopped:
+            sleep(duration)
+            self.speed = 0
+            self.start()
 
     def stop(self) -> None:
         """Stop emitting particles."""
