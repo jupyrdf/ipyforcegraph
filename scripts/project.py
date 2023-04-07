a244af82b7ade297e555a4bf07bd5df639685bd5
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2023-04-07 09:30:35 -0500

    restore bqplot

--- scripts/project.py
+++ scripts/project.py
@@ -148,7 +148,6 @@ PY = ["python"]
 PYM = [*PY, "-m"]
 PIP = [*PYM, "pip"]
 PIP_CHECK_IGNORE = [
-    r"bqplot.*pandas",
     r"conda\.cli\.main_run",
     r"No broken requirements found",
     r"sphinx-rtd-theme.*docutils",
